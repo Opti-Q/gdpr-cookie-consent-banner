522b1a9b0c3c2a601707d1bec8f0d0faac5086d3
amarek <alexander.marek@outlook.com>
2021-10-21 10:38:25 +0200

    switched from svg to font awesome and adapted size on narrower screens

--- dist/browser/bundle.js
+++ dist/browser/bundle.js
@@ -590,19 +590,7 @@
     	return {
     		c() {
     			button = element("button");
-
-    			button.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M510.52 255.82c-69.97-.85-126.47-57.69-126.47-127.86-70.17
-        0-127-56.49-127.86-126.45-27.26-4.14-55.13.3-79.72 12.82l-69.13
-        35.22a132.221 132.221 0 0 0-57.79 57.81l-35.1 68.88a132.645 132.645 0 0
-        0-12.82 80.95l12.08 76.27a132.521 132.521 0 0 0 37.16 72.96l54.77
-        54.76a132.036 132.036 0 0 0 72.71 37.06l76.71 12.15c27.51 4.36 55.7-.11
-        80.53-12.76l69.13-35.21a132.273 132.273 0 0 0
-        57.79-57.81l35.1-68.88c12.56-24.64 17.01-52.58 12.91-79.91zM176
-        368c-17.67 0-32-14.33-32-32s14.33-32 32-32 32 14.33 32 32-14.33 32-32
-        32zm32-160c-17.67 0-32-14.33-32-32s14.33-32 32-32 32 14.33 32 32-14.33
-        32-32 32zm160 128c-17.67 0-32-14.33-32-32s14.33-32 32-32 32 14.33 32
-        32-14.33 32-32 32z"></path></svg>`;
-
+    			button.innerHTML = `<i class="fa fa-user-secret fa-lg" aria-hidden="true"></i>`;
     			attr(button, "class", "cookieConsentToggle");
     		},
     		m(target, anchor) {
@@ -639,7 +627,7 @@
     	};
     }
 
-    // (173:0) {#if shown}
+    // (161:0) {#if shown}
     function create_if_block_2(ctx) {
     	let div4;
     	let div3;
@@ -747,7 +735,7 @@
     	};
     }
 
-    // (199:0) {#if settingsShown}
+    // (187:0) {#if settingsShown}
     function create_if_block(ctx) {
     	let div2;
     	let div1;
@@ -871,7 +859,7 @@
     	};
     }
 
-    // (203:6) {#if Object.hasOwnProperty.call(choicesMerged, choice.id) && choicesMerged[choice.id]}
+    // (191:6) {#if Object.hasOwnProperty.call(choicesMerged, choice.id) && choicesMerged[choice.id]}
     function create_if_block_1(ctx) {
     	let div;
     	let input;
@@ -962,7 +950,7 @@
     	};
     }
 
-    // (202:4) {#each choicesArr as choice}
+    // (190:4) {#each choicesArr as choice}
     function create_each_block(ctx) {
     	let show_if = Object.hasOwnProperty.call(/*choicesMerged*/ ctx[8], /*choice*/ ctx[30].id) && /*choicesMerged*/ ctx[8][/*choice*/ ctx[30].id];
     	let if_block_anchor;
